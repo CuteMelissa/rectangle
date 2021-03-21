9f22075078e03337925aba3dc5212dd80325f9a8
刘紫璐 <44942900+CuteMelissa@users.noreply.github.com>
2021-03-21 10:01:44 +0800

    Create main.js

--- main.js
+++ main.js
@@ -3,15 +3,57 @@ $(() => {
       $height    = $('#height'),
       $btnCal    = $('#calc')
       $perimeter = $('#perimeter'),
+      $widthValidate=$('#width-validate'),
+      $heightValidate=$('#height-validate'),
       $area      = $('#area');
+  
+  $width.keypress((e)=>{
+  let key=e.key,
+  val=e.target.value,
+  pos=e.target.selectionSatrt;
+
+
+  val=val.slice(0,pos)+key+val.slice(pos,val.length);
+
+  if(!/^(0|[1-9]\d*)(\.\d+)?((e|E)(\+|-)?\d+)?$/.test(val))e.preventDefault();
+
+  })
+  
+  $height.keypress((e)=>{
+  let key=e.key,
+  val=e.target.value,
+  pos=e.target.selectionSatrt;
+  val=val.slice(0,pos)+key+val.slice(pos,val.length);
+
+  if(!/^(0|[1-9]\d*)(\.\d+)?((e|E)(\+|-)?\d+)?$/.test(val))e.preventDefault();
+
+
+  })
+
+  $width.focusout(()=>{
+    if(!validate($width,$widthValidate)){
+      $width.select();
+    }
+  })
+
+  $height.focusout(()=>{
+    if(!validate($height,$heightValidate)){
+      $height.select();
+    }
+ 
+  })
+
 
   // calc button click
   $btnCal.click(()=> {
 
     let w = Number($width.val()),
         h = Number($height.val());
-
-    let p = (w + h) * 2;
+    
+    if(validate($width,$widthValidate)&&validate($height,$heightValidate
+    )){
+     //calc
+     let p = (w + h) * 2;
     let a = w * h;
 
     function roundFractional(x, n) {
@@ -31,6 +73,45 @@ $(() => {
     
     $perimeter.val(roundFractional(p,Math.max(n1,n2)));
     $area.val(roundFractional(a,n1+n2));
+
+    }
     
   });
 });
+
+function validate(input,output){
+  //isempty
+  if(input.val()===''){
+    output.html('该字段不能为空');
+    return false;
+  }
+  else{
+    output.html('');
+   
+  }
+
+
+
+
+//isnumber
+ let val=Number(input.val())
+ console.log(val);
+  if(isNaN(val)){
+    output.html('该字段应该是数字');
+    return false;
+  }else{
+  output.html('');
+  
+   }
+;
+
+if(val<0){
+  output.html('该字段不能小于0');
+  return false
+}else{
+  output.html('');
+ }
+
+
+return true;
+}
